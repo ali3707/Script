f37fbab0d1fb18a6570def304ddf48b1ab26bdd5
ali3707 <66162729+ali3707@users.noreply.github.com>
2021-01-21 17:20:45 +0800

    Update JD_DailyBonus.js

--- JD-DailyBonus/JD_DailyBonus.js
+++ JD-DailyBonus/JD_DailyBonus.js
@@ -15,7 +15,7 @@
 
 开启抓包app后, Safari浏览器登录 https://bean.m.jd.com/bean/signIndex.action 点击签到并且出现签到日历后, 返回抓包app搜索关键字 functionId=signBean 复制请求头Cookie填入以下Key处的单引号内即可 */
 
-var Key = ''; //单引号内自行填写您抓取的Cookie
+var Key = 'pt_pin=17862903759_p;pt_key=AAJgCTk8ADAe-pa3s-1xcm6u9vNpnzUUv-al5ZwkLmow-9A0CJ0U4QRiGskJ1jdb-ghG4J3jdKY;'; //单引号内自行填写您抓取的Cookie
 
 var DualKey = ''; //如需双账号签到,此处单引号内填写抓取的"账号2"Cookie, 否则请勿填写
 
